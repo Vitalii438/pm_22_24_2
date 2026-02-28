3d00f1cdb23062a9ab3b7dcd638afab235da9c57
Vitalii438 <Vitali.Somyk@gmail.com>
2026-02-28 18:19:04 +0200

    bootstrap

--- test/src/app/app.ts
+++ test/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -10,3 +11,4 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('test');
 } 
+ 
